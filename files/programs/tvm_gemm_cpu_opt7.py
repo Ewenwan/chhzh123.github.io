4ae0b3140fcf72f5d3dbfb7c9ea5817a249e0478
chhzh123 <982270930@qq.com>
2020-03-20 12:18:34 +0800

    [blog] Update TVM GEMM (parallelism)

---
+++ files/programs/tvm_gemm_cpu_opt7.py
@@ -0,0 +1,72 @@
+import tvm # v0.6
+import numpy as np
+import timeit
+
+# The size of the matrix
+# (M, K) x (K, N)
+# You are free to try out different shapes, sometimes TVM optimization outperforms numpy with MKL.
+M = 1024
+K = 1024
+N = 1024
+
+# The default tensor type in tvm
+dtype = "float32"
+
+# using Intel AVX2(Advanced Vector Extensions) ISA for SIMD
+# To get the best performance, please change the following line
+# to llvm -mcpu=core-avx2, or specific type of CPU you use
+target = 'llvm'
+ctx = tvm.context(target, 0)
+
+# ground truth
+a = tvm.nd.array(np.random.rand(M, K).astype(dtype), ctx)
+b = tvm.nd.array(np.random.rand(K, N).astype(dtype), ctx)
+c = tvm.nd.array(np.zeros((M, N), dtype=dtype), ctx)
+answer = np.dot(a.asnumpy(), b.asnumpy())
+
+###################
+# TVM part
+# Algorithm
+k = tvm.reduce_axis((0, K), 'k')
+A = tvm.placeholder((M, K), name='A')
+B = tvm.placeholder((K, N), name='B')
+# We have to re-write the algorithm slightly.
+bn = 32
+packedB = tvm.compute((N / bn, K, bn), lambda x, y, z: B[y, x * bn + z], name='packedB')
+C = tvm.compute((M, N),
+                lambda x, y: tvm.sum(A[x, k] * packedB[y // bn, k, tvm.indexmod(y, bn)], axis=k),
+                name = 'C')
+
+s = tvm.create_schedule(C.op)
+
+# Allocate write cache
+CC = s.cache_write(C, 'global')
+
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], x_factor=bn, y_factor=bn)
+
+# Write cache is computed at yo
+s[CC].compute_at(s[C], yo)
+# New inner axes
+xc, yc = s[CC].op.axis
+
+k, = s[CC].op.reduce_axis
+ko, ki = s[CC].split(k, factor=4)
+s[CC].reorder(ko, xc, ki, yc)
+s[CC].unroll(ki)
+s[CC].vectorize(yc)
+
+# parallel
+s[C].parallel(xo)
+
+x, y, z = s[packedB].op.axis
+s[packedB].vectorize(z)
+s[packedB].parallel(x)
+
+func = tvm.build(s, [A, B, C], target=target, name='mmult')
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=1)
+print('Opt7: %f' % evaluator(a, b, c).mean)
