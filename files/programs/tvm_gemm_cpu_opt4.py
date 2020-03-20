8112f4725f5cfe2bb5d543a88a358110e4c2e9cd
chhzh123 <982270930@qq.com>
2020-03-20 11:45:18 +0800

    [blog] Update TVM GEMM (vectorization)

---
+++ files/programs/tvm_gemm_cpu_opt4.py
@@ -0,0 +1,58 @@
+import tvm # v0.6
+import numpy as np
+import timeit
+
+# The size of the matrix
+# (M, K) x (K, N)
+# You are free to try out different shapes, sometimes TVM optimization outperforms numpy with MKL.
+M = 1024
+K = 1024
+N = 1024
+
+# The default tensor type in tvm
+dtype = "float32"
+
+# using Intel AVX2(Advanced Vector Extensions) ISA for SIMD
+# To get the best performance, please change the following line
+# to llvm -mcpu=core-avx2, or specific type of CPU you use
+target = 'llvm'
+ctx = tvm.context(target, 0)
+
+# ground truth
+a = tvm.nd.array(np.random.rand(M, K).astype(dtype), ctx)
+b = tvm.nd.array(np.random.rand(K, N).astype(dtype), ctx)
+c = tvm.nd.array(np.zeros((M, N), dtype=dtype), ctx)
+answer = np.dot(a.asnumpy(), b.asnumpy())
+
+###################
+# TVM part
+# Algorithm
+k = tvm.reduce_axis((0, K), 'k')
+A = tvm.placeholder((M, K), name='A')
+B = tvm.placeholder((K, N), name='B')
+C = tvm.compute((M, N),
+           lambda x, y: tvm.sum(A[x, k] * B[k, y], axis=k),
+           name='C')
+
+s = tvm.create_schedule(C.op)
+
+# Blocking by loop tiling
+bn = 64
+xo, yo, xi, yi = s[C].tile(C.op.axis[0], C.op.axis[1], x_factor=bn, y_factor=bn)
+k, = s[C].op.reduce_axis
+ko, ki = s[C].split(k, factor=8)
+
+# Hoist reduction domain outside the blocking loop
+s[C].reorder(xo, yo, ko, xi, ki, yi)
+
+# Vectorization
+s[C].vectorize(yi)
+
+func = tvm.build(s, [A, B, C], target=target, name='mmult')
+print(tvm.lower(s, [A, B, C], simple_mode=True))
+
+func(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), answer, rtol=1e-5)
+
+evaluator = func.time_evaluator(func.entry_name, ctx, number=1)
+print('Opt4: %f' % evaluator(a, b, c).mean)
